b579c1e32370265ec3041b8f78a9c8ac886ec44a
eviluser7 <eviluser7@gmail.com>
2020-09-12 18:09:12 -0300

    Add more things to player class
    No methods were added, however the inner code of these were modified.
    Here's a description of what happens in each:
    
    - __init__:
    - set x, y, vx, vy, direction, idle right and left sprite, and moving
      flag
    
    - draw:
    - draws the sprite property
    
    - update:
    - makes the character move along as checking the sprite and changing
      values
    
    - change_direction:
    - self-explanatory

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ resource.path = ['../resources', '../resources/img']
 resource.reindex()
 
 window = pyglet.window.Window(800, 600, caption="Pato Goes For a Walk")
-duck = resource.image('duck_idle_right.png')
+duck_idle_right = resource.image('duck_idle_right.png')
+duck_idle_left = resource.image('duck_idle_left.png')
 default_cur = window.get_system_mouse_cursor(window.CURSOR_DEFAULT)
 choose_cur = window.get_system_mouse_cursor(window.CURSOR_HAND)
 
@@ -17,7 +18,8 @@ def center_image(img):
     img.anchor_y = img.height // 2
 
 
-center_image(duck)
+center_image(duck_idle_right)
+center_image(duck_idle_left)
 
 
 # Code structure
@@ -49,22 +51,46 @@ class Game:
 class Player:
 
     def __init__(self):
-        pass
+        self.x = 400
+        self.y = 300
+        self.vx = 0
+        self.vy = 0
+        self.direction = 1
+        self.i_right = sprite.Sprite(duck_idle_right)
+        self.i_left = sprite.Sprite(duck_idle_left)
+        self.sprite = self.i_right
+        self.moving = False
 
     def draw(self):
-        pass
+        self.sprite.draw()
 
     def update(self, dt):
-        pass
+        new_x = self.x + self.vx * dt
+        new_y = self.y + self.vy * dt
 
-    def change_direction(self, vx, vy, dir):
-        pass
+        # Check sprite
+        if not self.moving and \
+           self.direction == 1:
+            self.sprite = self.i_right
+
+        if not self.moving and \
+           self.direction == 0:
+            self.sprite = self.i_left
+
+        self.x = new_x
+        self.y = new_y
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+
+    def change_direction(self, vx, vy, direction):
+        self.vx = vx
+        self.vy = vy
+        self.direction = direction
 
 
 @window.event
 def on_draw():
     window.clear()
-    duck.blit(400, 300)
 
 
 pyglet.app.run()
