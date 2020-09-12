e4f28841b1cfac283e36794635df8ab07a6ed625
eviluser7 <eviluser7@gmail.com>
2020-09-12 17:44:10 -0300

    Add game class
    Works as an engine for the game to work properly.
    Handles:
    - scene drawing
    - scene updating
    - mouse detection
    - clicking on scenes
    - changing scenes

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ resource.reindex()
 
 window = pyglet.window.Window(800, 600, caption="Pato Goes For a Walk")
 duck = resource.image('duck_idle_right.png')
+default_cur = window.get_system_mouse_cursor(window.CURSOR_DEFAULT)
+choose_cur = window.get_system_mouse_cursor(window.CURSOR_HAND)
 
 
 def center_image(img):
@@ -15,6 +17,32 @@ def center_image(img):
 
 
 center_image(duck)
+
+
+# Code structure
+class Game:
+
+    def __init__(self, scene):
+        self.scene = scene
+        self.mouse_x = 0
+        self.mouse_y = 0
+
+    def draw(self):
+        self.scene.draw()
+
+    def update(self, dt):
+        window.set_mouse_cursor(default_cur)
+        self.scene.update(dt)
+
+    def mouse_xy(self, x, y, dx, dy):
+        self.mouse_x = x
+        self.mouse_y = y
+
+    def on_click(self, x, y, button):
+        self.scene.on_click(x, y, button)
+
+    def set_scene_to(self, scene):
+        self.scene = scene
 
 
 @window.event
