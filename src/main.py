351faf51412cb4c20d83115d92c9f1185f76c8d7
eviluser7 <eviluser7@gmail.com>
2020-09-12 17:26:18 -0300

    Modify main
    Add center_image method

--- src/main.py
+++ src/main.py
@@ -9,10 +9,18 @@ window = pyglet.window.Window(800, 600, caption="Pato Goes For a Walk")
 duck = resource.image('duck_idle_right.png')
 
 
+def center_image(img):
+    img.anchor_x = img.width // 2
+    img.anchor_y = img.height // 2
+
+
+center_image(duck)
+
+
 @window.event
 def on_draw():
     window.clear()
-    duck.blit(0, 0)
+    duck.blit(400, 300)
 
 
 pyglet.app.run()
