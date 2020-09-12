84140a37d6c568f077a3070693e0a1a73b222033
eviluser7 <eviluser7@gmail.com>
2020-09-12 20:11:13 -0300

    Implement duck walk animation

--- src/main.py
+++ src/main.py
@@ -50,7 +50,33 @@ class Game:
 
 class Player:
 
+    walk_left_frames = [
+        resource.image('duck_walk_left_1.png'),
+        resource.image('duck_idle_left.png'),
+    ]
+
+    walk_right_frames = [
+        resource.image('duck_walk_right_1.png'),
+        resource.image('duck_idle_right.png'),
+    ]
+
+    walk_left = pyglet.image.Animation.from_image_sequence(walk_left_frames,
+                                                           duration=0.1,
+                                                           loop=True)
+
+    walk_right = pyglet.image.Animation.from_image_sequence(walk_right_frames,
+                                                            duration=0.1,
+                                                            loop=True)
+
     def __init__(self):
+        for img in self.walk_left_frames:
+            img.anchor_x = img.width // 2
+            img.anchor_y = img.height // 2
+
+        for img in self.walk_right_frames:
+            img.anchor_x = img.width // 2
+            img.anchor_y = img.height // 2
+
         self.x = 400
         self.y = 300
         self.vx = 0
@@ -58,6 +84,8 @@ class Player:
         self.direction = 1
         self.i_right = sprite.Sprite(duck_idle_right)
         self.i_left = sprite.Sprite(duck_idle_left)
+        self.w_right = sprite.Sprite(self.walk_right)
+        self.w_left = sprite.Sprite(self.walk_left)
         self.sprite = self.i_right
         self.moving = False
 
@@ -76,6 +104,14 @@ class Player:
         if not self.moving and \
            self.direction == 0:
             self.sprite = self.i_left
+
+        if self.moving and \
+           self.direction == 1:
+            self.sprite = self.w_right
+
+        if self.moving and \
+           self.direction == 0:
+            self.sprite = self.w_left
 
         self.x = new_x
         self.y = new_y
@@ -120,39 +156,50 @@ class ParkScene(Scene):
         # Linear movement
         if keys[key.W]:
             duck.change_direction(0, 170, duck.direction)
+            duck.moving = True
 
         if keys[key.A]:
             duck.change_direction(-170, 0, 0)
+            duck.moving = True
 
         if keys[key.S]:
             duck.change_direction(0, -170, duck.direction)
+            duck.moving = True
 
         if keys[key.D]:
             duck.change_direction(170, 0, 1)
+            duck.moving = True
 
         # Diagonal movement
         if keys[key.W] and keys[key.A]:
             duck.change_direction(-170, 170, 0)
+            duck.moving = True
 
         if keys[key.W] and keys[key.D]:
             duck.change_direction(170, 170, 1)
+            duck.moving = True
 
         if keys[key.S] and keys[key.A]:
             duck.change_direction(-170, -170, 0)
+            duck.moving = True
 
         if keys[key.S] and keys[key.D]:
             duck.change_direction(170, -170, 1)
+            duck.moving = True
 
         # Stop if two keys are pressed at the same time
         if keys[key.W] and keys[key.S]:
             duck.change_direction(0, 0, duck.direction)
+            duck.moving = False
 
         if keys[key.A] and keys[key.D]:
             duck.change_direction(0, 0, duck.direction)
+            duck.moving = False
 
         # If no key is pressed
         if not self.is_key_pressed():
             duck.change_direction(0, 0, duck.direction)
+            duck.moving = False
 
     def on_click(self, x, y, button):
         pass
